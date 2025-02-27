4a1040709e14492ec9dbb726e5ffe75a3c067d50
Anant Patel <66427020+anantdark@users.noreply.github.com>
2025-02-27 09:51:35 +0530

    product-of-array-except-self

--- gfg/longest-subarray-with-sum/longest-subarray-with-sum.py
+++ gfg/longest-subarray-with-sum/longest-subarray-with-sum.py
@@ -2,15 +2,26 @@ from typing import List
 
 class Solution:
     def longestSubarray(self, arr, k):  
-        pass
+        i = 0
+        j = len(arr) - 1
+        current_sum = sum(arr[i:j+1])
+        while i < j or current_sum > k:
+            if current_sum == k:
+                return j - i + 1
+            if arr[i] < arr[j]:
+                current_sum -= arr[i]
+                i += 1
+            else:
+                current_sum -= arr[j]
+                j -= 1
+        return 0
 
-    
 
 
 
 ## 
         # print(nums)
-arr = [10, 5, 2, 7, 1, -10]
+arr = [1, 2, 3, 10, 5, 2, 7, 1, 0, 0, -10, 2]
 k = 15
 ak = Solution().longestSubarray(arr, k)
 print(ak)
